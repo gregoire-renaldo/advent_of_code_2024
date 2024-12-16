19d8071db445f0d07d9bcc7daf883f0aa0085f95
gregoire renaldo <gregoire.renaldo@gmail.com>
2024-12-16 17:32:24 +0100

    part 1

--- 05/example.rb
+++ 05/example.rb
@@ -1,8 +1,27 @@
-lines = File.readlines('input_1.txt').map do |line|
-  # Split each line by the delimiter '|' and create a hash
-  parts = line.strip.split('|')
-  { parts[0].to_i => parts[1].to_i }
-end
+rules = [
+{47=>53},
+{97=>13},
+{97=>61},
+{97=>47},
+{75=>29},
+{61=>13},
+{75=>53},
+{29=>13},
+{97=>29},
+{53=>29},
+{61=>53},
+{97=>53},
+{61=>29},
+{47=>13},
+{75=>47},
+{97=>75},
+{47=>61},
+{75=>61},
+{47=>29},
+{75=>13},
+{53=>13}
+]
+
 
 rules = lines.each_with_object({}) do |h, acc|
   h.each do |k, v|
@@ -11,14 +30,26 @@ rules = lines.each_with_object({}) do |h, acc|
   end
 end
 
-arrays = File.readlines('input_2.txt').map do |line|
-  line.strip.split(',').map(&:to_i)
-end
-puts arrays[1]
+# rules = {
+#   47=>[53, 13, 61, 29], 
+#   97=>[13, 61, 47, 29, 53, 75], 
+#   75=>[29, 53, 47, 61, 13], 
+#   61=>[13, 53, 29], 
+#   29=>[13], 53=>[29, 13]
+# }
+
+# arrays = [
+#   [75,47,61,53,29],
+#   [97,61,53,29,13],
+#   [75,29,13],
+#   [75,97,47,61,53],
+#   [61,13,29],
+#   [97,13,75,29,47]
+# ]
+# 
 
 good_lines = []
 values = []
-
 
 def valid_arrays(arrays, rules)
   arrays.select do |array|
